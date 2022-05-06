64c86b61aa34ec85075339a84cff14e114f8c301
Gabriel Machado <gabriel.machado@totvs.com.br>
2022-05-05 21:19:12 -0300

    removing files after analyze

--- api/thirdparty.py
+++ api/thirdparty.py
@@ -5,19 +5,23 @@ from zipfile import ZipFile
 import lizard
 import os
 import requests
+import shutil
 
 api = Namespace("", description="Github integration")
 
 @api.route('/<string:username>/<string:repository>')
 class GithubIntegration(Resource):
     def get(self, username, repository):
-        resp = requests.get(f"https://api.github.com/repos/{username}/{repository}/zipball/master?recursive=1")
+        resp = requests.get(f"https://api.github.com/repos/{username}/{repository}/zipball/master")
 
         try:
             with ZipFile(BytesIO(resp.content)) as zf:
                 zf.extractall(f'__temp_code/{username}/{repository}');
 
             result = os.popen(f"lizard ./__temp_code/{username}/{repository}").read().splitlines()
+
+            shutil.rmtree(f"__temp_code/", ignore_errors=True)
+
             return result;
 
         except Exception as error:
